0a1854195e67efb3c9b6fe842119ff7c402ee15d
DelfimS <33528521+DelfimS@users.noreply.github.com>
2018-07-27 09:38:00 +0100

    levar para santa

--- routes/search.js
+++ routes/search.js
@@ -5,6 +5,7 @@ const searchReq = require('../modules/services/searchReq.js');
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var searchRes = searchReq.makeSearch(req.query.search);
+    res.render()
     res.send(searchRes);
 });
 
